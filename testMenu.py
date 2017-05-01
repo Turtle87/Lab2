acaeeff6dcb078e214e7bcbb515189d0a87e62aa
Joshua DeNio <deniojo@mnstate.edu>
2017-05-01 16:15:46 -0500

    Test program

---
+++ testMenu.py
@@ -0,0 +1,12 @@
+from createMenu import *
+def main():
+    tmp = ["Option One","Option two","Option three"]
+    print(createMenu(tmp))
+    tmp1 = ["Option One.1","Option two.2","Option three.3"]
+    print(createMenu(tmp))
+    tmp2 = ["Option One.0","Option two.0","Option three.0"]
+    print(createMenu(tmp))
+
+
+main()
+    
